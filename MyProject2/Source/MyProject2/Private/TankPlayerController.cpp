8eb32980c918b17335cc451cf85a135b6a26e63c
BioMannequin <ashton-mills@outlook.com>
2019-09-05 16:42:33 +0100

    Set up line trace for aimiing and logs hit end point

--- MyProject2/Source/MyProject2/Private/TankPlayerController.cpp
+++ MyProject2/Source/MyProject2/Private/TankPlayerController.cpp
@@ -26,3 +26,60 @@ void ATankPlayerController::BeginPlay()
 	}
 
 }
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimAtCrosshair();
+
+}
+
+void ATankPlayerController::AimAtCrosshair()
+{
+	if (!MyTank) { return; }
+	FVector HitLocation; //out parameter
+	GetSightRayHitLocation(HitLocation);
+	//Get world location if raycast through crosshair
+	//if it hits landscape
+	  //tell controlled tank to aim at theis point
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) 
+{
+	//find crosshair position in pixels
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	FVector2D ScreenLocation = FVector2D(ViewportSizeX*CrosshairXLocation, ViewportSizeY*CrosshairYLocation);
+	
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection))
+	{
+		//Raycast along that Lookdirecton and see what we hit (up to Max range)
+		FVector HitLocation;
+		GetLookVectorHitLocation(LookDirection,HitLocation);
+		UE_LOG(LogTemp, Warning, TEXT("HitDirection: %s "), *HitLocation.ToString());
+	}
+	HitLocation = FVector(0);
+	return true;
+}
+
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) 
+{
+	FVector WorldLocation;
+	return DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, WorldLocation, LookDirection);
+}
+
+bool ATankPlayerController::GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation)
+{
+	//make the raycast by single channnl
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+	FHitResult Hit;
+	auto StartLocation = PlayerCameraManager->GetCameraLocation();
+	auto EndLocation = StartLocation + (LookDirection * AimRange);
+	if (GetWorld()->LineTraceSingleByChannel(Hit, StartLocation,EndLocation,ECollisionChannel::ECC_Visibility))
+	{
+		HitLocation = Hit.Location;
+		return true;
+	}
+	else return false;
+}
--- MyProject2/Source/MyProject2/Public/TankPlayerController.h
+++ MyProject2/Source/MyProject2/Public/TankPlayerController.h
@@ -5,6 +5,9 @@
 #include "CoreMinimal.h"
 #include "Tank.h"
 #include "GameFramework/PlayerController.h"
+#include "Runtime/Engine/Public/DrawDebugHelpers.h"
+#include "Runtime/Engine/Public/CollisionQueryParams.h"
+#include "Runtime/Engine/Classes/Engine/World.h"
 #include "TankPlayerController.generated.h"
 
 /**
@@ -17,8 +20,24 @@ class MYPROJECT2_API ATankPlayerController : public APlayerController
 private:
 	ATank* MyTank = nullptr;
 
+	UPROPERTY (EditAnywhere)
+	float CrosshairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.33333;
+
+	UPROPERTY(EditAnywhere)
+		int32 AimRange = 1000000;
+
 public : 
 	ATank* GetControlledTank() const;
 	void SetControlledTank();
 	void BeginPlay() override;
+	void Tick(float DeltaTime) override;
+	//Start the tank moving the barrel so that a shot would hit
+	//where the crosshair intersects the world
+	void AimAtCrosshair();
+	bool GetSightRayHitLocation(FVector &HitLocation);
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection);
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector &HitLocation);
 };
